f780b705580bdc4857b3e1d16863f9a76726ede4
Manny2296 <manuelfelip-96@hotmail.com>
2017-01-31 19:20:17 -0500

    First Commit

---
+++ src/main/java/com/springboot/spa/SprngbootaSpaApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngbootaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngbootaSpaApplication.class, args);
+	}
+}
